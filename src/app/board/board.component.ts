27101cc54661284d506af996b6a76477e065107a
Jan Suchotzki <jan@lernmoment.de>
2016-10-20 13:50:35 +0200

    Adds existing game functionality

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,11 +6,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent{
+  private board: string[][] = [["", "", ""], ["", "", ""], ["", "", ""]];
   private player: string = "x";
-  constructor() {
-    let that = this;
-    setTimeout(function() {
-      that.player = "o";
-    }, 2000);
+  constructor() { }
+
+  private setToken(i: number, j: number) {
+    if (!this.isWon() && !this.isDraw()) {
+      console.log("setToken: " + i + " " + j);
+      if (this.board[i][j] === "") {
+        this.board[i][j] = this.player;
+        if (!this.isWon()) {
+          if (this.player === "x") {
+            this.player = "o";
+          } else {
+            this.player = "x";
+          }
+        }
+      }
+    }
+  }
+
+  private isDraw(): boolean {
+    if (this.isWon()) {
+      return false;
+    }
+    for (let i = 0; i < this.board.length; i++) {
+      for (let j = 0; j < this.board.length; j++) {
+        if (this.board[i][j] === "") {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  private isWon(): boolean {
+    if (
+      (this.board[0][0] !== "" && this.board[0][0] === this.board[0][1] && this.board[0][0] === this.board[0][2])
+      || (this.board[1][0] !== "" && this.board[1][0] === this.board[1][1] && this.board[1][0] === this.board[1][2])
+      || (this.board[2][0] !== "" && this.board[2][0] === this.board[2][1] && this.board[2][0] === this.board[2][2])
+      || (this.board[0][0] !== "" && this.board[0][0] === this.board[1][0] && this.board[0][0] === this.board[2][0])
+      || (this.board[0][1] !== "" && this.board[0][1] === this.board[1][1] && this.board[0][1] === this.board[2][1])
+      || (this.board[0][2] !== "" && this.board[0][2] === this.board[1][2] && this.board[0][2] === this.board[2][2])
+      || (this.board[0][0] !== "" && this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2])
+      || (this.board[2][0] !== "" && this.board[2][0] === this.board[1][1] && this.board[2][0] === this.board[0][2])
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private getGameState(): number {
+    if (this.isWon()) {
+      return 2;
+    }
+    if (this.isDraw()) {
+      return 1;
+    }
+    return 0;
   }
 }
